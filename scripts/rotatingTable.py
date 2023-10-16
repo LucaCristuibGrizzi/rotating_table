c9f3e687f31b0715244ed0a5968507d9b7f77461
LucaCristuibGrizzi <luca.cristuibgrizzi@gmail.com>
2023-10-16 15:29:05 +0200

    Fix cleanup

--- scripts/rotatingTable.py
+++ scripts/rotatingTable.py
@@ -13,9 +13,9 @@ in3 = 27
 in4 = 22
 
 # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.005
+step_sleep = 0.001
 
-step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°(85/15= 5.6667 -> 5.6667/2 = 2.8333 -> 2.8333*4096 = ~ 11605)
+step_count = 11605 # 5.625*(1/64) per step, 4096 steps is 360°(85/15= 5.6667 -> 5.6667/2 = 2.8333 -> 2.8333*4096 = ~ 11605)
 
 direction = False # True for clockwise, False for counter-clockwise
 
@@ -44,41 +44,41 @@ GPIO.output( in3, GPIO.LOW )
 GPIO.output( in4, GPIO.LOW )
 
 motor_pins = [in1,in2,in3,in4]
-motor_step_counter = 0
+
 
 def cleanup():
+    lowOutputPin()
+    GPIO.cleanup()
+
+def lowOutputPin():
     GPIO.output( in1, GPIO.LOW )
     GPIO.output( in2, GPIO.LOW )
     GPIO.output( in3, GPIO.LOW )
     GPIO.output( in4, GPIO.LOW )
-    GPIO.cleanup()
 
 def rotate(data):
-    try:
-        i = 0
-        for i in range(step_count):
-            for pin in range(0, len(motor_pins)):
-                GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
-            if direction==True:
-                motor_step_counter = (motor_step_counter - 1) % 8
-            elif direction==False:
-                motor_step_counter = (motor_step_counter + 1) % 8
-            else: # defensive programming
-                print( "uh oh... direction should *always* be either True or False" )
-                cleanup()
-                exit( 1 )
-            time.sleep( step_sleep )
+        
+    motor_step_counter = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+        if direction==True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction==False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else: # defensive programming
+            print( "uh oh... direction should *always* be either True or False" )
+            cleanup()
+            exit( 1 )
+        time.sleep( step_sleep )
 
-    except KeyboardInterrupt:
-        cleanup()
-        exit( 1 )
-
-    cleanup()
+    lowOutputPin()
     pub.publish("OK")
     exit( 0 )
 
 def listener():
     rospy.init_node('rotating_table', anonymous=True)
+    rospy.on_shutdown(cleanup)
     rospy.Subscriber('cmd_rotating_table', String, rotate)
     rospy.spin()
 
