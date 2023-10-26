395aa6a54bce018dadc15a5614ad6e3e3a6902ae
LucaCristuibGrizzi <luca.cristuibgrizzi@gmail.com>
2023-10-26 09:57:12 +0200

    Add slowdown error

--- scripts/rotatingTable.py
+++ scripts/rotatingTable.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool, Int16
 import RPi.GPIO as GPIO
 import time
+import threading, sys
 
-pub = rospy.Publisher('rs_rotating_table', String, queue_size=10)
+pub = rospy.Publisher('rs_rotating_table', Bool, queue_size=1)
 
 in1 = 17
 in2 = 18
 in3 = 27
 in4 = 22
+
+speedLock = threading.Lock()
+componentErrorLock = threading.Lock()
+
+componentError = False
+nComponentError = 0
+counterComponentError = 0
 
 # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
 step_sleep = 0.001
@@ -45,6 +53,24 @@ GPIO.output( in4, GPIO.LOW )
 
 motor_pins = [in1,in2,in3,in4]
 
+def getSpeed():
+    with speedLock :
+        return step_sleep
+    
+def setSpeed(sleep):
+    with speedLock :
+        global step_sleep
+        step_sleep = sleep
+
+def getCounterComponentError():
+    with componentErrorLock:
+        return counterComponentError
+    
+def addCounterComponentError():
+    with componentErrorLock:
+        global counterComponentError
+        counterComponentError =+ 1
+
 
 def cleanup():
     lowOutputPin()
@@ -57,8 +83,13 @@ def lowOutputPin():
     GPIO.output( in4, GPIO.LOW )
 
 def rotate(data):
-        
+    if False:
+        speed = 60
+        pub.publish(str(speed))
+        return
+
     motor_step_counter = 0
+    sleep = getSpeed()
     for i in range(step_count):
         for pin in range(0, len(motor_pins)):
             GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
@@ -70,17 +101,35 @@ def rotate(data):
             print( "uh oh... direction should *always* be either True or False" )
             cleanup()
             exit( 1 )
-        time.sleep( step_sleep )
+        time.sleep( sleep )
 
     lowOutputPin()
-    pub.publish("OK")
+    pub.publish(True)
     exit( 0 )
+
+def setSpeed(data):
+    max_step_sleep = 0.002
+    min_step_sleep = 0.001
+
+    sleep = max_step_sleep + (float(data.data - 1) * ((min_step_sleep - max_step_sleep)/99.0))
+    setSpeed(sleep)
+    print(sleep)
 
 def listener():
     rospy.init_node('rotating_table', anonymous=True)
     rospy.on_shutdown(cleanup)
     rospy.Subscriber('cmd_rotating_table', String, rotate)
+    rospy.Subscriber('slowdown_error', Int16, setSpeed)
     rospy.spin()
 
 if __name__ == "__main__":
+    for av in sys.argv[1:]:
+        errorType = av[:2]
+        if errorType == "-c":
+            try:
+                nComponentError = int(av[2]) - 1
+                componentError = True
+            except:
+                print("Error: no number in the argument")
+    setSpeed(0.001)
     listener()
