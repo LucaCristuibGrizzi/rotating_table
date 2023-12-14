d7596d821d206c581902a18ac1d0782f0ba64e74
LucaCristuibGrizzi <luca.cristuibgrizzi@gmail.com>
2023-12-14 22:04:34 +0100

    Add comments and README

---
+++ README.md
@@ -0,0 +1,74 @@
+# Rotating Table
+
+This package contains the script to control the rotating table and communicate its status with the production line.
+
+For this project, a specific rotating table was designed. The 3D printed components have a minor effect on the implementation of the code (specifically, the number of steps needed to complete a specific rotation), but, in general, the code can be used to control any type of rotating table built with the same electronic components. Therefore, the focus will be on these components.
+
+This is the rotating table built for this project:
+
+![Rotating Table](images/RotatingTable.png)
+
+The electronic components are the following:
+- **Stepper Motor 28BYJ-48**
+- **Driver ULN2003**
+- **Raspberry Pi 3 B+**
+
+On the Raspberry Pi, the OS *Ubuntu Server 20.04 LTS* is installed, as it allows the installation of ROS, which is used for communication between this device and the production line.
+
+## Installation
+
+The first step is to install the OS *Ubuntu Server 20.04 LTS*. This [tutorial](https://ubuntu.com/tutorials/how-to-install-ubuntu-on-your-raspberry-pi#1-overview "tutorial Ubuntu Server") explains how to do it. For the initial power-on, it is advisable to use an Ethernet connection to the Internet because Wi-Fi connection is not guaranteed. For the SSH connection, this command has fewer issues:
+
+
+```bash
+ssh <username>@<Raspberry Pi’s IP address>
+```
+
+Now, install ROS following the standard procedure. Look at this [tutorial](https://medium.com/geekculture/robot-operating-system-installation-configuration-and-auto-startup-on-a-raspberry-pi-with-ubuntu-6eb8e4e1038e "tutorial ROS on Ubuntu Server") for more information.
+
+After ROS installation, clone the following repository into your ROS workspace:
+
+```bash
+git clone https://github.com/LucaCristuibGrizzi/rotating_table
+```
+
+Build you workspace:
+
+```bash
+catkin_make # or catkin build
+```
+
+## Setup
+
+The correct wiring is crucial for making the rotating table work. The connections between the GPIO pins of the header of the Raspberry Pi and the driver inputs are as follows:
+- GPIO 17 with IN1
+- GPIO 18 with IN2
+- GPIO 27 with IN3
+- GPIO 22 with IN4
+
+The power supply is connected directly to the appropriate pins of the Raspberry Pi. However, note that these pins lack protection from shortcuts or voltage spikes. Therefore, it is essential to ensure that the power supply is precise and stable. The following image shows the wiring used:
+
+![Wiring](images/ElectricCircuit.png)
+
+A highly recommended tip for simplifying the execution of ROS across multiple machines is to add the following commands to the *.bashrc* file of both the PC and the Raspberry Pi. These commands are necessary for running ROS across multiple machines, but you can choose to execute them every time you open a terminal or do it automatically by adding these commands to the *.bashrc* file:
+
+```bash
+export ROS_MASTER_URI=http://<ROS Master device’s IP address>:11311
+export ROS_IP=<device’s IP address>
+```
+
+## Additional Information
+
+The stepper motor 28BYJ-48, controlled in half-step, needs 64 steps to complete one revolution of the input shaft. The internal gearbox of the motor has a ratio of 1/64. So, 4096 steps are needed to execute a revolution of the output shaft. In the rotating table, a driving gear with 15 teeth is applied to the motor ($Z_{\textit{driving gear}} = 15$), and the top table has 85 internal teeth ($Z_{\textit{top table}} = 85$). This allows us to compute the motor steps needed for a rotation of the desired angle. In this case, the angle is 180°, and the resulting steps are 11605.
+
+The computation is described by the following equation:
+
+$$ \textit{Step Output Shaft} = \frac{\textit{Step Input Shaft}}{\textit{Gearbox Ratio}} = \frac{64}{\frac{1}{64}} = 4096 \textit{ steps}$$
+
+$$ \textit{Step For 180\degree Rotation} = \textit{Step Output Shaft} \cdot \frac{Z_{\textit{top table}}}{Z_{\textit{driving gear}}} \cdot \frac{\textit{Desired Angle}}{360\degree} = \\ = 4096 \cdot \frac{85}{15} \cdot \frac{180\degree}{360\degree} = 11605 \textit{ steps}$$
+
+## Libraries Version
+
+- **ROS Noetic**: 1.16.0
+- **Pyhton**: 3.8.10
+- **RPi.GPIO**: 0.7.1
--- scripts/rotatingTable.py
+++ scripts/rotatingTable.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
-
 import rospy
-from std_msgs.msg import String, Bool, Int16
+from std_msgs.msg import String, Int16
 import RPi.GPIO as GPIO
 import time
 import threading, sys
 
+#--------- GLOBAL VARIABLE ---------#
+# Publishers needed for communicating with the production line node.
 pub = rospy.Publisher('rs_rotating_table', String, queue_size=10)
 
-in1 = 17
-in2 = 18
-in3 = 27
-in4 = 22
-
-speedLock = threading.Lock()
-componentErrorLock = threading.Lock()
-
+# Variables used to generate errors at the specified time
+# during the node launch.
+slowdownError = False
+nSlowdownError = 0
+counterSlowdownError = 0
 componentError = False
 nComponentError = 0
 counterComponentError = 0
 
-# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
-step_sleep = 0.001
+# Lock variable for all the global variables that are shared across multiple threads
+# and might be accessed at the same time.
+speedLock = threading.Lock()
+componentErrorLock = threading.Lock()
+slowdownErrorLock = threading.Lock()
 
-step_count = 11605 # 5.625*(1/64) per step, 4096 steps is 360°(85/15= 5.6667 -> 5.6667/2 = 2.8333 -> 2.8333*4096 = ~ 11605)
+# Initializing variable concenring the control of the stepper motor by the Raspberry Pi.
 
-direction = False # True for clockwise, False for counter-clockwise
+# Define how quick the rotatinga table is moving.
+# Careful lowering this, mechanical limitation of the motor can create issue.
+step_sleep = 0.001 # This value is near to minimum limit that can be reached.
+# Step to execute a 180° rotation. Look at the README file to understad how to compute this value. 
+step_count = 11605
+# True for clockwise, False for counter-clockwise 
+direction = False 
 
-# defining stepper motor sequence 
-# (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+# Define stepper motor sequence.
 step_sequence = [[1,0,0,1],
                  [1,0,0,0],
                  [1,1,0,0],
@@ -38,21 +44,32 @@ step_sequence = [[1,0,0,1],
                  [0,0,1,1],
                  [0,0,0,1]]
 
-# setting up
+# Define the GPIO pins connected to the input of the ULN2003 driver.
+in1 = 17 # GPIO 17
+in2 = 18 # GPIO 18
+in3 = 27 # GPIO 27
+in4 = 22 # GPIO 22
+
+# Select the "Broadcom SOC channel" number mode to refer to the GPIO.
 GPIO.setmode( GPIO.BCM )
+
+# Set up the GPIO as pinout.
 GPIO.setup( in1, GPIO.OUT )
 GPIO.setup( in2, GPIO.OUT )
 GPIO.setup( in3, GPIO.OUT )
 GPIO.setup( in4, GPIO.OUT )
 
-# initializing
+# Initialize the GPIO pins to a LOW state.
 GPIO.output( in1, GPIO.LOW )
 GPIO.output( in2, GPIO.LOW )
 GPIO.output( in3, GPIO.LOW )
 GPIO.output( in4, GPIO.LOW )
 
+# Define the list of the motor pins.
 motor_pins = [in1,in2,in3,in4]
 
+#--------- FUNCTION ---------#
+# Functions for getting and setting variables shared across multiple threads.
 def getStepSleep():
     with speedLock :
         return step_sleep
@@ -61,6 +78,15 @@ def setStepSleep(sleep):
     with speedLock :
         global step_sleep
         step_sleep = sleep
+
+def getCounterSlowdownError():
+    with slowdownErrorLock:
+        return counterSlowdownError
+    
+def addCounterSlowdownError():
+    with slowdownErrorLock:
+        global counterSlowdownError
+        counterSlowdownError += 1
 
 def getCounterComponentError():
     with componentErrorLock:
@@ -71,60 +97,73 @@ def addCounterComponentError():
         global counterComponentError
         counterComponentError += 1
 
-
+# Function to execute the necessary cleanup operations.
 def cleanup():
     lowOutputPin()
     GPIO.cleanup()
 
+# Function to set the specified pins to a LOW state.
 def lowOutputPin():
     GPIO.output( in1, GPIO.LOW )
     GPIO.output( in2, GPIO.LOW )
     GPIO.output( in3, GPIO.LOW )
     GPIO.output( in4, GPIO.LOW )
 
+# Function that controls the rotation of the rotating table.
 def rotate(data):
-    if componentError and nComponentError == getCounterComponentError():
-        addCounterComponentError()
-        lowOutputPin()
-        pub.publish("OK")
-        exit(0)
-        return
-    
-    addCounterComponentError()
 
-    if False:
-        speed = 60
+    # Generate specified errors during node launch for the robot.
+    if (slowdownError and nSlowdownError == getCounterSlowdownError()):
+        addCounterSlowdownError()
+        speed = 50
         pub.publish(str(speed))
         return
+    elif (componentError and nComponentError == getCounterComponentError()):
+        addCounterComponentError()
+        pub.publish("OK")
+        return
 
-    motor_step_counter = 0
+    addCounterSlowdownError()
+    addCounterComponentError()
+
+    # Start the rotation. 
+    motor_step_counter = 0 # Used to apply the right step sequence.
     sleep = getStepSleep()
     for i in range(step_count):
+
+        # Apply the correct state to the pins.
         for pin in range(0, len(motor_pins)):
             GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+
+        # Keep track of the correct step sequence to apply based on the rotating direction.
         if direction==True:
             motor_step_counter = (motor_step_counter - 1) % 8
         elif direction==False:
             motor_step_counter = (motor_step_counter + 1) % 8
-        else: # defensive programming
+        else:
             print( "uh oh... direction should *always* be either True or False" )
             cleanup()
             exit( 1 )
+
+        # Wait to realize the next step.
         time.sleep( sleep )
 
+    # When the rotation is completed, publish the message.
     lowOutputPin()
     pub.publish("OK")
     exit( 0 )
 
+# Function to set the speed (and thus the step sleep) of the rotating table.
+# Note: The speed changes in a linear way between two extremes.
 def setSpeed(data):
     max_step_sleep = 0.002
     min_step_sleep = 0.001
 
     sleep = max_step_sleep + (float(data.data - 1) * ((min_step_sleep - max_step_sleep)/99.0))
     setStepSleep(sleep)
-    print("set")
-    print(sleep)
 
+# Function that defines the ROS node and sets up subscribers for communication with
+# the production line node and the FlexBE behavior "Production Line".  
 def listener():
     rospy.init_node('rotating_table', anonymous=True)
     rospy.on_shutdown(cleanup)
@@ -133,13 +172,28 @@ def listener():
     rospy.spin()
 
 if __name__ == "__main__":
+
+    # When launching the node, arguments can be used to define the type
+    # of error to generate and when to generate it.
+    # s -> slowdown error
+    # c -> component not arrived error
     for av in sys.argv[1:]:
         errorType = av[:2]
-        if errorType == "-c":
+        if errorType == "-s":
+            try:
+                nSlowdownError = int(av[2]) - 1
+                slowdownError = True
+            except:
+                print("Error: no number in the argument")
+        elif errorType == "-c":
             try:
                 nComponentError = int(av[2]) - 1
                 componentError = True
             except:
                 print("Error: no number in the argument")
+
+    # Set the rotating table speed to its maximum.
     setStepSleep(0.001)
+
+    # Initialize the ROS node and start the subscribers.
     listener()
